5b54eedc5f55088505a787f8f7d8b1523bebbf77
cetecom <cetecom@LCT50421>
2019-04-03 22:17:31 -0300

    Creación de estructura principal del sitio

--- index.php
+++ index.php
@@ -6,12 +6,81 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="UTF-8">  
+        <?php include('./lib/constantes.php'); ?>        
+        <title><?=sitename;?></title>        
+        <?php include('./frame.php');?>
+        
     </head>
     <body>
         <?php
-        // put your code here
+        
         ?>
+ 
+        <div id="img">
+            <img src="img/Logo_DuocUC.jpg" alt="" height="100px" width="135px"/>
+        </div>
+        <div id="titulo">            
+            <center><h2><?=sitename?></h2></center>
+        </div>
+
+        <div id="menu">
+            <?php include('./menu.php'); ?>
+        </div>
+        
+ 
+        <div id="titcontenido">
+            <center><h3>cuerpo de Pagina</h3></center>
+        </div>
+        <div id="contenido">
+            <p>cuerpo de la pagina !</p>
+        </div>
     </body>
+<script>
+        $("#prisolicitudA").on( "click", function( event ) {
+            if(($("[id*=subsolicitudA]").css("display")!="none")){
+                 $("[id*=subsolicitudA]").css("display","none");
+            }
+            else{
+                $("[id*=subsolicitudA]").show();
+                $("[id*=subsolicitudA]").css("display","block");
+            }
+                
+                
+            });
+        
+        $('[data-toggle="tooltip"]').tooltip(); 
+    </script>
+    
+        <script>
+        $("#prisolicitudB").on( "click", function( event ) {
+            if(($("[id*=subsolicitudB]").css("display")!="none")){
+                 $("[id*=subsolicitudB]").css("display","none");
+            }
+            else{
+                $("[id*=subsolicitudB]").show();
+                $("[id*=subsolicitudB]").css("display","block");
+            }
+                
+                
+            });
+        
+        $('[data-toggle="tooltip"]').tooltip(); 
+    </script>
+    
+        <script>
+        $("#prisolicitudC").on( "click", function( event ) {
+            if(($("[id*=subsolicitudC]").css("display")!="none")){
+                 $("[id*=subsolicitudC]").css("display","none");
+            }
+            else{
+                $("[id*=subsolicitudC]").show();
+                $("[id*=subsolicitudC]").css("display","block");
+            }
+                
+                
+            });
+        
+        $('[data-toggle="tooltip"]').tooltip(); 
+    </script>
 </html>
---
+++ menu.php
@@ -0,0 +1,14 @@
+<div class="list-group">
+<a class="list-group-item" href="<?=URLBASE;?>"><i class="fa fa-home fa-2x"></i> Home</a>
+<a id="prisolicitudA" class="list-group-item" href="#" data-toggle="tooltip" data-placement="right" title="Consultas"><i class="fa fa-file-o fa-2x"></i> About Chile</a>
+<!--<a id="subsolicitudA" class="list-group-item submenu" href="<?=URLBASE;?>Contenido/Consultas/ConsultaProductos.php" ><i class="fa fa-sun-o fa-2x"></i> Consulta Productos</a>-->
+
+<a id="prisolicitudB" class="list-group-item" href="<?=URLBASE;?>contenidos/aboutChile.php" data-toggle="tooltip" data-placement="right" title="About Chile"><i class="fa fa-file-o fa-2x"></i> About Chile</a>
+<!--<<a id="subsolicitudB" class="list-group-item submenu" href="<?=URLBASE;?>Contenido/Informes/InformeVentas.php" ><i class="fa fa-sun-o fa-2x"></i> Informe Ventas</a>-->
+
+<a id="prisolicitudC" class="list-group-item" href="<?=URLBASE;?>contenidos/reservation.php" data-toggle="tooltip" data-placement="right" title="Reservation"><i class="fa fa-file-o fa-2x"></i> Reservations</a>
+<!--<<a id="subsolicitudC" class="list-group-item submenu" href="<?=URLBASE;?>Contenido/Ingresos/IngresoBodegas.php" ><i class="fa fa-sun-o fa-2x"></i> Ingreso Bodegas</a>-->
+<!--<<a id="subsolicitudC" class="list-group-item submenu" href="<?=URLBASE;?>Contenido/Ingresos/IngresoProductos.php" ><i class="fa fa-sun-o fa-2x"></i> Ingreso Productos</a>-->
+<!--<<a id="subsolicitudC" class="list-group-item submenu" href="<?=URLBASE;?>Contenido/Ingresos/IngresoVentas.php" ><i class="fa fa-sun-o fa-2x"></i> Ingreso Ventas</a>-->
+
+</div>
